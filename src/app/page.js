8a42fa737035c03565adf1db3f83f3068ffd965c
gustimartin <gustavoemartin1991@gmail.com>
2023-06-18 18:20:35 -0300

    h

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Hero from "@/components/Hero";
-import Projects from "@/components/projects";
+import Projects from "@/components/Projects";
 import { ToastContainer } from "react-toastify";
 export default function Home() {
   return (
