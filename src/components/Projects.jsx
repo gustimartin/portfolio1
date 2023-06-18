a620b9ff3047829476d0ddb319b6d1fd60ced9b3
gustimartin <gustavoemartin1991@gmail.com>
2023-06-18 20:06:39 -0300

    w4

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ import thecleanplate from "../../public/thecleanplate.png";
 import henrycountry from "../../public/henrycountry.png";
 function Projects() {
   return (
-    <div id="projects" className=" w-full mt-28 ">
+    <div id="projects" className=" w-full mt-24 ">
       <div className=" max-w-[1240] mx-auto px-2 py-16 ">
         <p className=" uppercase tracking-widest text-4xl">Projects</p>
         <div className=" grid md:grid-cols-2 gap-8">
