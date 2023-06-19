9135b7c0a03f688b34211bc09a8d61fa376f5f58
gustimartin <gustavoemartin1991@gmail.com>
2023-06-19 14:26:41 -0300

    w

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 import Image from "next/legacy/image";
 function ProjectItem({ title, bgImg, projectUrl }) {
   return (
-    <div className=" relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-md p-4 group hover:bg-gray-900">
+    <div className=" relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 dark:hover-bg-gray-950 dark:shadow-lg dark:shadow-gray-700 rounded-md p-4 group hover:bg-gray-900">
       <Image
         src={bgImg}
         alt="/"
         className=" rounded-sm group-hover:opacity-10"
       />
       <div className=" hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] ">
-        <h3 className=" text-2xl text-white tracking-wider text-center ">
+        <h3 className=" text-2xl text-white tracking-wider text-center  ">
           {title}
         </h3>
         <p className=" pb-4 pt-2 text-white text-center ">React js</p>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,9 +6,11 @@ import thecleanplate from "../../public/thecleanplate.png";
 import henrycountry from "../../public/henrycountry.png";
 function Projects() {
   return (
-    <div id="projects" className=" w-full mt-24 ">
+    <div id="projects" className=" w-full mt-24 dark:bg-slate-800">
       <div className=" max-w-[1240] mx-auto px-2 py-16 ">
-        <p className=" uppercase tracking-widest text-4xl">Projects</p>
+        <p className=" uppercase tracking-widest text-4xl dark:text.gray-200">
+          Projects
+        </p>
         <div className=" grid md:grid-cols-2 gap-8">
           <ProjectItem
             title="The Clean Plate"
