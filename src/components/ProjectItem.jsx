454bca8bf517768deebed126c105105c6f611058
gustimartin <gustavoemartin1991@gmail.com>
2023-08-23 15:16:30 -0300

    now

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -9,7 +9,7 @@ function ProjectItem({ title, bgImg, projectUrl }) {
         className=" rounded-sm group-hover:opacity-10 dark:bg-gray-950"
       />
       <div className=" hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] ">
-        <h3 className=" text-2xl text-white tracking-wider text-center  ">
+        <h3 className=" text-2xl text-white tracking-wider text-center p-4  ">
           {title}
         </h3>
 
