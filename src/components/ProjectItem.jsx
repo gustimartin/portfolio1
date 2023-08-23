98124a12e2c2ceb88e861f87ee49b56b529f29a2
gustimartin <gustavoemartin1991@gmail.com>
2023-08-23 15:02:41 -0300

    change

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -12,7 +12,7 @@ function ProjectItem({ title, bgImg, projectUrl }) {
         <h3 className=" text-2xl text-white tracking-wider text-center  ">
           {title}
         </h3>
-        <p className=" pb-4 pt-2 text-white text-center ">React js</p>
+
         <Link href={projectUrl}>
           <p className=" text-center p-2 rounded-md bg-gray-200 text-gray-900 font-bold text-lg">
             see more
